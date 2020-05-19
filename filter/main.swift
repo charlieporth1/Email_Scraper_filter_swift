3bfeed7554e43ad1cfa48aa233e2e1d61f59aa43
Charlie Porth <charlieporth1@gmail.com>
2020-05-19 11:53:39 -0500

    Initial Commit

---
+++ filter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  filter
+//
+//  Created by Charlie  Porth on 5/19/20.
+//  Copyright © 2020 Charlie  Porth. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
